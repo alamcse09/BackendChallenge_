10d245bd1c9d529f9d94082c1eb5f121cf0e8665
Ashraful Alam <alam.cse09@gmail.com>
2018-06-24 15:41:32 +0600

    Configuration added

--- src/main/java/com/n26/challenge/config/Config.java
+++ src/main/java/com/n26/challenge/config/Config.java
@@ -6,12 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import com.google.gson.Gson;
 import com.n26.challenge.statistics.service.StatisticsService;
 import com.n26.challenge.statistics.service.StatisticsServiceImpl;
+import com.n26.challenge.time.Time;
 import com.n26.challenge.transaction.service.TransactionService;
 import com.n26.challenge.transaction.service.TransactionServiceImpl;
 
 @Configuration
 public class Config {
 
+	public static final int SECONDS_TO_CONSIDER = 60;
+	public static final int SECOND_TO_MILLISECOND_MULTIPLIER = 1000;
+	public static final long TEN_DAYS_IN_SECOND = 60L * 60L * 24L * 10L;
+	public static final int NUM_OF_TRANSACTIONS_FOR_TEST = 100000;
+	
+	@Bean
+	public Time time() {
+		
+		return new Time();
+	}
+	
 	@Bean
 	public Gson gson() {
 		
